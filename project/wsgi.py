44795bffbc56d9267c73a267798b888d296d3159
Tyler Grantham <tjaygrantham@gmail.com>
2020-08-14 16:59:22 -0400

    Move socket.io to python server

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import socketio
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-application = get_wsgi_application()
+sio = socketio.Server()
+
+application = socketio.WSGIApp(sio, get_wsgi_application())
+
+@sio.event
+def connect(sid, env):
+    print("socket connected:", sid)
