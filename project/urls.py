5ad8e62dd29c92c69cd726b98eb36f9d60e1210a
Rh017548 <russellhill752@msn.com>
2020-08-14 15:35:38 -0400

    allauth added with Google/Github FB pending

--- project/urls.py
+++ project/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
 
+
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
