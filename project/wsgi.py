a5977779da081c58b5e41ae02e4d9d4c62ac268e
Tyler Grantham <tjaygrantham@gmail.com>
2020-08-16 12:07:29 -0400

    Add stream to event page

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,6 +11,7 @@ import os
 import socketio
 
 from django.core.wsgi import get_wsgi_application
+from core.models import Event
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
@@ -18,6 +19,29 @@ sio = socketio.Server()
 
 application = socketio.WSGIApp(sio, get_wsgi_application())
 
+# Join event room
 @sio.event
-def connect(sid, env):
-    print("socket connected:", sid)
+def join_event(sid, eventId, userId, peerId):
+    event = Event.objects.filter(pk=eventId).first()
+    print(userId, "joining", eventId)
+    sio.save_session(sid, { 'userId': userId, 'eventId': eventId, 'peerId': peerId})
+    sio.enter_room(sid, eventId)
+    sio.emit('user-connected', peerId, to=eventId, skip_sid=sid)
+
+# Send chat message. Possibly save as comment?
+@sio.event
+def send_chat(sid, message):
+    session = sio.get_session(sid)
+    sio.emit
+    sio.emit('recieve-chat-message', {'userId': session['userId']}, to=session['eventId'])
+
+# Leave room on disconnect
+@sio.event
+def disconnect(sid):
+    session = sio.get_session(sid)
+    eventId = session['eventId']
+    userId = session['userId']
+    peerId = session['peerId']
+    print(peerId)
+    print(userId, "leaving", eventId)
+    sio.leave_room(sid, eventId)
---
+++ react/components/Event.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { render } from 'react-dom'
+import io from 'socket.io-client'
+import Peer from 'peerjs'
+import _ from 'lodash'
+import Webcam from 'react-webcam'
+import ReactPlayer from 'react-player'
+
+/* global data */
+
+const PORT = process.env.PORT || 3000
+const props = JSON.parse(_.unescape(data))
+
+export default class Event extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      isOwner: props.ownerId === props.userId,
+      streaming: false,
+      player: null,
+      in_progress: false,
+      socket: null,
+      peer: null,
+      viewers: 0
+    }
+  }
+
+  componentWillUnmount () {
+    this.state.socket.close()
+  }
+
+  async componentDidMount () {
+    await this.setState({
+      socket: io(`http://192.168.1.80:${PORT}`),
+      peer: new Peer({
+        host: '192.168.1.80',
+        port: PORT,
+        path: '/peer',
+        secure: PORT !== 3000
+      })
+    })
+    const { socket, peer, isOwner } = this.state
+    peer.on('open', id => {
+      socket.emit('join_event', props.pk, props.userId, id)
+      if (isOwner) {
+        const peers = {}
+        navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true
+        }).then(stream => {
+          this.setState({
+            player: <Webcam audio='false' mirrored='true' />
+          })
+          socket.on('user-connected', peerId => {
+            const call = peer.call(peerId, stream)
+            console.log(peerId, 'connected')
+            peers[peerId] = call
+            this.setState({
+              viewers: this.state.viewers + 1
+            })
+            call.on('close', () => {
+              this.setState({
+                viewers: this.state.viewers - 1
+              })
+            })
+          })
+        })
+      } else {
+        peer.on('call', (call, id) => {
+          console.log('answering call')
+          call.answer()
+          call.on('stream', stream => {
+            console.log('got stream, setting player')
+            this.setState({
+              player: <ReactPlayer url={stream} playing={true} controls={true} />
+            })
+          })
+          call.on('close', () => {
+            socket.close()
+          })
+        })
+      }
+    })
+    window.addEventListener('beforeunload', e => {
+      socket.close()
+    })
+  }
+
+  render () {
+    return (
+      <>
+        {this.state.player}
+      </>
+    )
+  }
+}
+
+const container = document.querySelector('#react-event')
+if (container) render(<Event />, container)
