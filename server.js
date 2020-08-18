c29adcfe4d62f1e4570fb8ec5bba3e367edc171a
Tyler Grantham <tjaygrantham@gmail.com>
2020-08-17 23:27:43 -0400

    Modify headers so allauth gets the right uri

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const { ExpressPeerServer } = require('peer')
 app.use('/peer', ExpressPeerServer(server))
 app.use('/static/css', express.static('node_modules/antd/dist'))
 app.use('/', proxy('http://localhost:8000', {
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    // Modify headers if running on Heroku
+    proxyReqOpts.headers['X-Forwarded-Proto'] = PORT === 3000 ? 'http' : 'https'
+    proxyReqOpts.headers.Host = PORT === 3000 ? 'localhost:3000' : 'rhappsody.herokuapp.com'
+    return proxyReqOpts
+  },
   limit: '100mb'
 }))
 
