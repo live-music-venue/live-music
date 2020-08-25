5485a371f4fc2851bd3a1640cd3c0d3e103285d8
Tyler Grantham <tjaygrantham@gmail.com>
2020-08-25 15:09:22 -0400

    force https

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const proxy = require('express-http-proxy')
 const PORT = process.env.PORT || 3000
 const { ExpressPeerServer } = require('peer')
 
-// Redirect to https
+// Redirect to https if on Heroku
 function requireHTTPS (req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && PORT !== 3000) {
     return res.redirect('https://' + req.get('host') + req.url)
   }
   next()
