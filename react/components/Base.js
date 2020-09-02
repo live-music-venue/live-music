d86e81d1db218cab258696cc81456bdf06e6b9c6
Rajee Karunaratne <rajee.karunaratne@gmail.com>
2020-09-01 21:45:45 -0400

    js py and template translations complete

--- react/components/Base.js
+++ react/components/Base.js
@@ -137,7 +137,7 @@ export default class Base extends React.Component {
                     redirect('/')
                   }}
                 >
-                  Home
+                  {gettext('Home')}
                 </Menu.Item>
 
                 {!isMusician && isAuthenticated && (
@@ -148,7 +148,7 @@ export default class Base extends React.Component {
                       redirect(musicianSignupURL)
                     }}
                   >
-                    Become a Streamer!
+                    {gettext('Become a Streamer!')}
                   </Menu.Item>
                 )}
 
@@ -172,7 +172,7 @@ export default class Base extends React.Component {
                       redirect(addEventURL)
                     }}
                   >
-                    Add Event
+                    {gettext('Add Event')}
                   </Menu.Item>
                 )}
 
@@ -184,7 +184,7 @@ export default class Base extends React.Component {
                       redirect(favoritesURL)
                     }}
                   >
-                    Favorites
+                    {gettext('Favorites')}
                   </Menu.Item>
                 )}
 
@@ -195,7 +195,7 @@ export default class Base extends React.Component {
                       redirect(mapURL)
                     }}
                   >
-                    Map
+                    {gettext('Map')}
                   </Menu.Item>
 
 
@@ -206,7 +206,7 @@ export default class Base extends React.Component {
                     redirect(aboutUsURL)
                   }}
                 >
-                  About Band Together
+                  {gettext('About Band Together')}
                 </Menu.Item>
 
                 {!isAuthenticated && (
@@ -217,7 +217,7 @@ export default class Base extends React.Component {
                       redirect(loginURL)
                     }}
                   >
-                    Login
+                    {gettext('Login')}
                   </Menu.Item>
                 )}
 
@@ -229,7 +229,7 @@ export default class Base extends React.Component {
                       redirect(signupURL)
                     }}
                   >
-                    Register
+                    {gettext('Register')}
                   </Menu.Item>
                 )}
 
@@ -241,7 +241,7 @@ export default class Base extends React.Component {
                       redirect(logoutURL)
                     }}
                   >
-                    Logout
+                    {gettext('Logout')}
                   </Menu.Item>
                 )}
 
